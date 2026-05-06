d074abbf779e25e70e02826a3eaf48ba750d716d
Christoph Stäbler <cstabler@redhat.com>
2026-05-06 17:36:33 +0200

    cleanup: remove unnecessary backward compat fallback in entrypoint

--- sandbox/entrypoint.sh
+++ sandbox/entrypoint.sh
@@ -2,9 +2,6 @@
 if [ -n "${VCS_CREDENTIAL_URLS}" ]; then
   printf '%s\n' "${VCS_CREDENTIAL_URLS}" > ~/.git-credentials
   git config --global credential.helper store
-elif [ -n "${GITHUB_TOKEN}" ]; then
-  echo "https://x-access-token:${GITHUB_TOKEN}@github.com" > ~/.git-credentials
-  git config --global credential.helper store
 fi
 if [ -n "${GITHUB_TOKEN}" ]; then
   echo "${GITHUB_TOKEN}" | gh auth login --with-token 2>/dev/null || true
